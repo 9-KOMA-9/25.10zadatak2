76f44933a67ddb972505e39d49d2552590089914
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-10-25 08:28:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak25._10._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite nekakav tekst");
+            string s1=Console.ReadLine();
+            Console.WriteLine(s1.Replace(" ", "_"));
+            Console.ReadKey();
+        }
+    }
+}
